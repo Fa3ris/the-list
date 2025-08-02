d4d8d649eff1ff2e8f34eba6583589a9b3d9c3ff
Fabrice Tsiava <fabrice.tsiava@contentsquare.com>
2025-08-03 01:51:47 +0200

    enrich text + prettier

--- scripts/generate-site.spec.ts
+++ scripts/generate-site.spec.ts
@@ -1,166 +1,253 @@
-import { vol } from 'memfs';
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { vol } from "memfs";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 
-import { toSideBarNav, walkDir } from './generate-site.js';
-
+import { toSideBarNav, walkDir } from "./generate-site.js";
 
 vi.mock("fs", async () => {
-    const memfs = await vi.importActual("memfs");
-    return { default: memfs, ...memfs };
+  const memfs = await vi.importActual("memfs");
+  return { default: memfs, ...memfs };
 });
 
-describe('walkDir', () => {
-    beforeEach(() => {
-        vol.reset()
-        vol.fromNestedJSON(
-            {
-                '/docs': {
-                    '.vitepress': {},
-                    'public': {
-                        'icon.png': 'icon.png'
-                    },
-                    book: {
-                        'index.md': 'index.md',
-                        'foo.md': `---
+describe("walkDir", () => {
+  beforeEach(() => {
+    vol.reset();
+    vol.fromNestedJSON({
+      "/docs": {
+        ".vitepress": {},
+        public: {
+          "icon.png": "icon.png",
+        },
+        book: {
+          "index.md": "index.md",
+          "foo.md": `---
 Title: foo
 Status: ✅
 ---
-`
-                    },
-                    game: {
-                        action: {
-                            'baz.md': 'baz.md',
-                        },
-                    },
-                },
-            }
-        )
+`,
+        },
+        game: {
+          action: {
+            "baz.md": "baz.md",
+          },
+        },
+      },
     });
+  });
 
-    describe('filtering directories', () => {
-        it('should ignore .vitepress and public directories and non Markdown files', () => {
-            expect(walkDir('/docs')).toStrictEqual({
-                book: { foo: 'foo', index: 'index' },
-                game: { action: { baz: 'baz' } }
-            })
-        });
+  describe("filtering directories", () => {
+    it("should ignore .vitepress and public directories and non Markdown files", () => {
+      expect(walkDir("/docs")).toStrictEqual({
+        book: { foo: "foo", index: "index" },
+        game: { action: { baz: "baz" } },
+      });
     });
+  });
 });
 
+describe("toSideBarNav", () => {
+  it("transform nav to side bar navigation", () => {
+    expect(
+      toSideBarNav({
+        book: { foo: "foo", index: "index" },
+      })
+    ).toStrictEqual({
+      "/book": [
+        {
+          base: "/book",
+          text: "Book",
+          items: [
+            { text: "Index", link: "/index" },
+            { text: "Foo", link: "/foo" },
+          ],
+        },
+      ],
+    });
+  });
 
-describe('toSideBarNav', () => {
+  it("multiple entries", () => {
+    expect(
+      toSideBarNav({
+        book: { foo: "foo", index: "index" },
+        game: { baz: "baz" },
+      })
+    ).toStrictEqual({
+      "/book": [
+        {
+          base: "/book",
+          text: "Book",
+          items: [
+            { text: "Index", link: "/index" },
+            { text: "Foo", link: "/foo" },
+          ],
+        },
+      ],
+      "/game": [
+        {
+          base: "/game",
+          text: "Game",
+          items: [{ text: "Baz", link: "/baz" }],
+        },
+      ],
+    });
+  });
 
-    it('transform nav to side bar navigation', () => {
-        expect(toSideBarNav({
-            book: { foo: 'foo', index: 'index' },
-        })).toStrictEqual({
-            "/book": [
+  it("nested entries", () => {
+    expect(
+      toSideBarNav({
+        game: { action: { baz: "baz" } },
+      })
+    ).toStrictEqual({
+      "/game": [
+        {
+          base: "/game",
+          text: "Game",
+          items: [
+            {
+              base: "/game/action",
+              text: "Action",
+              items: [{ text: "Baz", link: "/baz" }],
+            },
+          ],
+        },
+      ],
+    });
+  });
+
+  it("multiple nested entries", () => {
+    expect(
+      toSideBarNav({
+        game: {
+          action: { baz: "baz" },
+          rpg: { japan: { pokemon: "pokemon" } },
+        },
+      })
+    ).toStrictEqual({
+      "/game": [
+        {
+          base: "/game",
+          text: "Game",
+          items: [
+            {
+              base: "/game/action",
+              text: "Action",
+              items: [{ text: "Baz", link: "/baz" }],
+            },
+            {
+              base: "/game/rpg",
+              text: "Rpg",
+              items: [
                 {
-                    base: '/book',
-                    text: 'Book',
-                    items: [
-                        { text: "Index", link: "/index" },
-                        { text: "Foo", link: "/foo" }
-                    ]
-                }
-            ]
-        })
-    })
+                  base: "/game/rpg/japan",
+                  text: "Japan",
+                  items: [{ text: "Pokemon", link: "/pokemon" }],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+  });
 
-    it('multiple entries', () => {
-        expect(toSideBarNav({
-            book: { foo: 'foo', index: 'index' },
-            game: { baz: 'baz' }
-        })).toStrictEqual({
-            "/book": [
-                {
-                    base: '/book',
-                    text: 'Book',
-                    items: [
-                        { text: "Index", link: "/index" },
-                        { text: "Foo", link: "/foo" }
-                    ]
-                }
-            ],
-            '/game': [{
-                base: '/game',
-                text: 'Game',
-                items: [
-                    { text: 'Baz', link: '/baz' }
-                ]
-            }]
-        })
-    })
+  it("ignore index if missing", () => {
+    expect(
+      toSideBarNav({
+        book: { foo: "foo" },
+      })
+    ).toStrictEqual({
+      "/book": [
+        {
+          base: "/book",
+          text: "Book",
+          items: [{ text: "Foo", link: "/foo" }],
+        },
+      ],
+    });
+  });
 
-    it('nested entries', () => {
-        expect(toSideBarNav({
-            game: { action: { baz: 'baz' } }
-        })).toStrictEqual({
-            "/game": [
-                {
-                    base: '/game',
-                    text: 'Game',
-                    items: [
-                        {
-                            base: '/game/action',
-                            text: 'Action',
-                            items: [
-                                { text: 'Baz', link: "/baz" }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        })
-    })
+  it("read file to set the title", () => {
+    vol.reset();
+    vol.fromNestedJSON({
+      "/docs": {
+        book: {
+          "the-fault-in-our-stars.md": `---
+Title: Nos Etoiles contraires
+Emoji: ✅⭐️
+---
+`,
+        },
+      },
+    });
 
-    it('multiple nested entries', () => {
-        expect(toSideBarNav({
-            game: { action: { baz: 'baz' }, rpg: { japan: { pokemon: 'pokemon' } } }
-        })).toStrictEqual({
-            "/game": [
-                {
-                    base: '/game',
-                    text: 'Game',
-                    items: [
-                        {
-                            base: '/game/action',
-                            text: 'Action',
-                            items: [
-                                { text: 'Baz', link: "/baz" }
-                            ]
-                        },
-                        {
-                            base: '/game/rpg',
-                            text: 'Rpg',
-                            items: [
-                                {
-                                    base: '/game/rpg/japan', text: 'Japan', items: [
-                                        { text: 'Pokemon', link: '/pokemon' }
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        })
-    })
+    expect(toSideBarNav(walkDir("/docs"), "/docs")).toStrictEqual({
+      "/book": [
+        {
+          base: "/book",
+          items: [
+            {
+              link: "/the-fault-in-our-stars",
+              text: "✅⭐️ Nos Etoiles contraires",
+            },
+          ],
+          text: "Book",
+        },
+      ],
+    });
+  });
 
-    it('ignore index if missing', () => {
-        expect(toSideBarNav({
-            book: { foo: 'foo', },
-        })).toStrictEqual({
-            "/book": [
-                {
-                    base: '/book',
-                    text: 'Book',
-                    items: [
-                        { text: "Foo", link: "/foo" }
-                    ]
-                }
-            ]
-        })
-    })
+  it("ignore if no frontmatter", () => {
+    vol.reset();
+    vol.fromNestedJSON({
+      "/docs": {
+        book: {
+          "the-fault-in-our-stars.md": ``,
+        },
+      },
+    });
 
-})
+    expect(toSideBarNav(walkDir("/docs"), "/docs")).toStrictEqual({
+      "/book": [
+        {
+          base: "/book",
+          items: [
+            {
+              link: "/the-fault-in-our-stars",
+              text: "The-fault-in-our-stars",
+            },
+          ],
+          text: "Book",
+        },
+      ],
+    });
+  });
+
+
+  it("ignores missing emoji", () => {
+    vol.reset();
+    vol.fromNestedJSON({
+      "/docs": {
+        book: {
+          "the-fault-in-our-stars.md": `---
+Title: Nos Etoiles contraires
+---
+`,
+        },
+      },
+    });
+
+    expect(toSideBarNav(walkDir("/docs"), "/docs")).toStrictEqual({
+      "/book": [
+        {
+          base: "/book",
+          items: [
+            {
+              link: "/the-fault-in-our-stars",
+              text: "Nos Etoiles contraires",
+            },
+          ],
+          text: "Book",
+        },
+      ],
+    });
+  });
+});
--- scripts/generate-site.ts
+++ scripts/generate-site.ts
@@ -1,137 +1,203 @@
-import path from 'path';
+import path from "path";
 
-import fs from 'fs';
-import type { DefaultTheme } from 'vitepress';
+import fs from "fs";
+import type { DefaultTheme } from "vitepress";
+import util from "node:util";
 
-const DOCS_DIR = path.resolve(process.env.INIT_CWD || path.dirname(import.meta.dirname), 'docs')
+const DOCS_DIR = path.resolve(
+  process.env.INIT_CWD || path.dirname(import.meta.dirname),
+  "docs"
+);
 
 type Doc = {
-    title: string;
-    article: string;
-}
+  title: string;
+  article: string;
+};
 
 const japan: Doc = {
-    title: 'Japan',
-    article: 'lovely'
-}
+  title: "Japan",
+  article: "lovely",
+};
 
 function generateVitePressMarkdown(item: Doc) {
-    return `---
+  return `---
 title: ${item.title}
 ---
 ${item.article}
 `;
 }
 
-
-
 function generateSite() {
-    const content = generateVitePressMarkdown(japan)
-    fs.writeFileSync(path.join(DOCS_DIR, `${japan.title}.md`), content);
+  const content = generateVitePressMarkdown(japan);
+  fs.writeFileSync(path.join(DOCS_DIR, `${japan.title}.md`), content);
 }
 
-
-type Filename = string
+type Filename = string;
 export type Nav = { [entry: string]: Nav | Filename };
 
-
-function walkDir(rootDir: string, options = {
+function walkDir(
+  rootDir: string,
+  options = {
     dirsToIgnore: [/\.vitepress/, /public/],
-    fileExtensionsToInclude: [/md/]
-}) {
+    fileExtensionsToInclude: [/md/],
+  }
+) {
+  const entries = fs.readdirSync(rootDir, {
+    recursive: true,
+    withFileTypes: true,
+  });
 
-    const entries = fs.readdirSync(rootDir, { recursive: true, withFileTypes: true });
+  const { dirsToIgnore, fileExtensionsToInclude } = options;
 
-    const { dirsToIgnore, fileExtensionsToInclude } = options
-
-    const filtered = entries.filter(entry => {
-
-        if (entry.isDirectory()) {
-            return !dirsToIgnore.some(p => p.test(entry.name)) && !dirsToIgnore.some(p => p.test(entry.parentPath))
-        }
-
-        return fileExtensionsToInclude.some(p => p.test(path.extname(entry.name))) && !dirsToIgnore.some(pattern => pattern.test(entry.parentPath))
-    })
-
-    const files: string[] = []
-    const dirs: string[] = []
-    for (const entry of filtered) {
-        const fullPath = path.resolve(entry.parentPath, entry.name);
-        if (entry.isFile()) {
-            files.push(fullPath)
-        } else if (entry.isDirectory()) {
-            dirs.push(fullPath)
-        }
+  const filtered = entries.filter((entry) => {
+    if (entry.isDirectory()) {
+      return (
+        !dirsToIgnore.some((p) => p.test(entry.name)) &&
+        !dirsToIgnore.some((p) => p.test(entry.parentPath))
+      );
     }
 
-    const dirPaths = dirs.map(dir => path.relative(rootDir, dir).split("/")).sort((p1, p2) => p1.length - p2.length)
+    return (
+      fileExtensionsToInclude.some((p) => p.test(path.extname(entry.name))) &&
+      !dirsToIgnore.some((pattern) => pattern.test(entry.parentPath))
+    );
+  });
 
-    const nav: Nav = {};
-
-    for (const dirPath of dirPaths) {
-        let currentDir = nav;
-        for (const dir of dirPath) {
-            currentDir[dir] = typeof currentDir[dir] === 'object' ? currentDir[dir] : {};
-            currentDir = currentDir[dir]
-        }
+  const files: string[] = [];
+  const dirs: string[] = [];
+  for (const entry of filtered) {
+    const fullPath = path.resolve(entry.parentPath, entry.name);
+    if (entry.isFile()) {
+      files.push(fullPath);
+    } else if (entry.isDirectory()) {
+      dirs.push(fullPath);
     }
+  }
 
+  const dirPaths = dirs
+    .map((dir) => path.relative(rootDir, dir).split("/"))
+    .sort((p1, p2) => p1.length - p2.length);
 
-    const relativeFilePaths = files.map(file => path.relative(rootDir, file).split('/'))
+  const nav: Nav = {};
 
-    for (const relativePath of relativeFilePaths) {
-        let currentDir = nav;
-        for (const entry of relativePath) {
-            if (typeof currentDir[entry] === 'object') {
-                currentDir = currentDir[entry]
-            } else {
-                const parsed = path.parse(entry)
-                currentDir[parsed.name] = parsed.name
-            }
-        }
+  for (const dirPath of dirPaths) {
+    let currentDir = nav;
+    for (const dir of dirPath) {
+      currentDir[dir] =
+        typeof currentDir[dir] === "object" ? currentDir[dir] : {};
+      currentDir = currentDir[dir];
     }
+  }
 
-    return nav;
+  const relativeFilePaths = files.map((file) =>
+    path.relative(rootDir, file).split("/")
+  );
+
+  for (const relativePath of relativeFilePaths) {
+    let currentDir = nav;
+    for (const entry of relativePath) {
+      if (typeof currentDir[entry] === "object") {
+        currentDir = currentDir[entry];
+      } else {
+        const parsed = path.parse(entry);
+        currentDir[parsed.name] = parsed.name;
+      }
+    }
+  }
+
+  return nav;
 }
-
 
 function capitalizeFirstLetter(str: string): string {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-function toSideBarNav(nav: Nav): DefaultTheme.Sidebar {
-    return Object.fromEntries(Object.entries(nav).map(([entryName, entryContent]) => {
-        if (typeof entryContent !== 'object') return []
-        return [`/${entryName}`, [toSideBarItem(entryName, entryContent, [])]];
-    }))
-
+function toSideBarNav(
+  nav: Nav,
+  rootDir: string | undefined = undefined
+): DefaultTheme.Sidebar {
+  return Object.fromEntries(
+    Object.entries(nav)
+      .map(([entryName, entryContent]) => {
+        if (typeof entryContent !== "object") return [];
+        return [
+          `/${entryName}`,
+          [toSideBarItem(entryName, entryContent, [], rootDir)],
+        ];
+      })
+      .filter((keyValue) => keyValue.length > 0)
+  );
 }
 
-function toSideBarItem(entryName: string, content: Filename | Nav, parentPath: string[]): DefaultTheme.SidebarItem {
-
-    if (typeof content === 'string') {
-        return { text: capitalizeFirstLetter(content), link: `/${content}` }
+function toSideBarItem(
+  entryName: string,
+  content: Filename | Nav,
+  parentPath: string[],
+  rootDir: string | undefined
+): DefaultTheme.SidebarItem {
+  if (typeof content === "string") {
+    const defaultText = capitalizeFirstLetter(content);
+    if (!rootDir) {
+      return { text: defaultText, link: `/${content}` };
     }
+    const bytesToRead = 500;
+    const buffer = Buffer.alloc(bytesToRead); // Read first 500 bytes
+    const pathToMarkdown = path.resolve(
+      rootDir,
+      ...parentPath,
+      `${content}.md`
+    );
+    const fd = fs.openSync(pathToMarkdown, "r");
+    const bytesRead = fs.readSync(fd, buffer, 0, bytesToRead, 0);
 
-    const items = Object.entries(content).map(entry =>
-        toSideBarItem(entry[0], entry[1], [...parentPath, entryName])
-    )
-
-    const indexFileIdx = items.findIndex(({ text }) => typeof text === 'string' && text.toLowerCase() === 'index')
-    const [indexFile] = indexFileIdx >= 0 ? items.splice(indexFileIdx, 1) : [undefined];
-
-    return {
-        base: `/${parentPath.length > 0 ? `${parentPath.join('/')}/` : ''}${entryName}`,
-        text: capitalizeFirstLetter(entryName),
-        items: indexFile ? [indexFile].concat(items) : items
+    if (bytesRead === 0) {
+      return { text: defaultText, link: `/${content}` };
     }
-}
+    fs.closeSync(fd);
+    const partialContent = buffer.subarray(0, bytesRead).toString("utf8");
 
+    const metadata = Object.fromEntries(
+      partialContent
+        .split("\n")
+        .filter((line) => line.includes(":"))
+        .map((line) => {
+          const [key, value] = line.split(":");
+          if (!key || !value) return undefined;
+          return [key.trim().toLowerCase(), value.trim()];
+        })
+        .filter((keyPair) => keyPair !== undefined)
+    );
+
+    const titleParts = [metadata.emoji ?? "", metadata.title ?? defaultText].filter(
+      (s) => s !== ""
+    );
+
+    return { text: titleParts.join(" "), link: `/${content}` };
+  }
+
+  const items = Object.entries(content).map((entry) =>
+    toSideBarItem(entry[0], entry[1], [...parentPath, entryName], rootDir)
+  );
+
+  const indexFileIdx = items.findIndex(
+    ({ text }) => typeof text === "string" && text.toLowerCase() === "index"
+  );
+  const [indexFile] =
+    indexFileIdx >= 0 ? items.splice(indexFileIdx, 1) : [undefined];
+
+  return {
+    base: `/${
+      parentPath.length > 0 ? `${parentPath.join("/")}/` : ""
+    }${entryName}`,
+    text: capitalizeFirstLetter(entryName),
+    items: indexFile ? [indexFile].concat(items) : items,
+  };
+}
 
 export { toSideBarNav, walkDir };
 
 // Run only if this file is executed directly
 if (process.argv[1] === import.meta.filename) {
-    walkDir(DOCS_DIR)
-    // generateSite()
+  const sideBar = toSideBarNav(walkDir(DOCS_DIR), DOCS_DIR);
+  console.log(util.inspect(sideBar, undefined, null));
 }
